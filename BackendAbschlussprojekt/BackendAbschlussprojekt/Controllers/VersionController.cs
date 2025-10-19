6e8d1982201a34e37e650021b738a446d6acdcaf
TsukiYami <tsukiyamigaming@gmail.com>
2025-10-20 01:16:42 +0200

    Changes

---
+++ BackendAbschlussprojekt/BackendAbschlussprojekt/Controllers/VersionController.cs
@@ -0,0 +1,24 @@
+﻿using BackendAbschlussprojekt.DB;
+using BackendAbschlussprojekt.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BackendAbschlussprojekt.Controllers
+{
+    [Route("/api/")]
+    [ApiController]
+    public class VersionController : ControllerBase
+    {
+        private VersionService m_oVersionService;
+
+        public VersionController(AufraumaktionContext oContext)
+        {
+            m_oVersionService = new VersionService(oContext);
+        }
+
+        [HttpGet]
+        public IActionResult GetAllVersions()
+        {
+
+        }
+    }
+}
